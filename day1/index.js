1fe0bcbea7078cf7c962873a6cce84039367eeb1
Nobuko <nobuko@next-data-service.com>
2021-12-02 21:19:22 +0100

    day1

---
+++ day1/index.js
@@ -0,0 +1,21 @@
+/**
+ * day 1 - first
+ * @param {Array<number>} texts
+ */
+const calculate = (texts) => {
+    // Check the number of items which are bigger than previous value
+    let lastNumber = null;
+    return texts.reduce((count, current, i)=>{
+        if (i !== 0 && lastNumber < parseInt(current)) {
+            count++;
+        }
+        lastNumber = parseInt(current);
+        return count;
+    }, [0])
+};
+
+const fs = require("fs");
+const file = fs.readFileSync("./day1/input.txt").toString('utf-8');
+const textArray = file.split("\n")
+
+console.log(calculate(textArray));
---
+++ day1/index2.js
@@ -0,0 +1,27 @@
+/**
+ * day2 - second
+ * @param {Array<number>} texts
+ */
+const calculate = (texts) => {
+    // create array of sum
+    const sumArray = [];
+    for (let i = 2; i < texts.length; i++) {
+        sumArray.push(parseInt(texts[i-2])+parseInt(texts[i-1])+parseInt(texts[i]));
+    }
+
+    // Check the number of items which are bigger than previous value
+    let previous = null;
+    return sumArray.reduce((count, current, i)=>{
+        if (i !== 0 && previous < parseInt(current)) {
+            count++;
+        }
+        previous = parseInt(current);
+        return count;
+    }, [0])
+};
+
+const fs = require("fs");
+const file = fs.readFileSync("./day1/input.txt").toString('utf-8');
+const textArray = file.split("\n")
+
+console.log(calculate(textArray));

17a986ed9a6c0fa3988c5d5f7230091b68147b1a
Nobuko <nobuko@next-data-service.com>
2021-12-05 10:41:17 +0100

    day5 - 1

---
+++ day5/index2.js
@@ -0,0 +1,14 @@
+/**
+ * day X - second
+ * @param {Array<string>} inputs
+ * @return {number}
+ */
+const calculate = (inputs) => {
+    return -1;
+};
+
+const fs = require("fs");
+const file = fs.readFileSync("./day5/sample.txt").toString('utf-8');
+const input = file.split("\n")
+
+console.log(calculate(input));
---
+++ day_template/index2.js
@@ -0,0 +1,15 @@
+/**
+ * day X - second
+ * @param {Array<string>} inputs
+ * @return {number}
+ */
+const calculate = (inputs) => {
+
+    return -1;
+};
+
+const fs = require("fs");
+const file = fs.readFileSync("./day4/input.txt").toString('utf-8');
+const input = file.split("\n")
+
+console.log(calculate(input));

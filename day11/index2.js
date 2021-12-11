3d0bec6fba0854ff6c38db822582c21b78150542
Nobuko <nobuko@next-data-service.com>
2021-12-11 11:05:22 +0100

    day11

---
+++ day11/index2.js
@@ -0,0 +1,109 @@
+/**
+ * day 11 - second
+ * @param {Array<Array<string>>} inputs
+ * @return {number}
+ */
+const calculate = (input, n) => {
+    let matrix = JSON.parse(JSON.stringify(input));
+    let result = 0;
+    let searching = true;
+    do {
+        // add self and adjacent +1
+        for (const [rowIndex, row] of matrix.entries()) {
+            for (let [colIndex, col] of row.entries()) {
+                if (matrix[rowIndex][colIndex] !== "X") {
+                    matrix[rowIndex][colIndex] += 1;
+                    if (matrix[rowIndex][colIndex] > 9) {
+                        matrix[rowIndex][colIndex] = "X"; // sign for flash
+                        matrix = flash(matrix, rowIndex, colIndex);
+                    }
+                }
+            }
+        }
+
+        // count > 9 octopus and reset to 0
+        let count = 0;
+        for (const [rowIndex, row] of matrix.entries()) {
+            for (let [colIndex, col] of row.entries()) {
+                if (col === "X") {
+                    matrix[rowIndex][colIndex] = 0;
+                    count += 1;
+                }
+            }
+        }
+        result += 1;
+        if (count === (matrix.length * matrix[0].length)) {
+            searching = false;
+        }
+    } while (searching);
+
+    return result;
+};
+
+const flash = (matrixOriginal, rowIndex, colIndex) => {
+    let matrix = JSON.parse(JSON.stringify(matrixOriginal));
+    if (matrix?.[rowIndex-1]?.[colIndex-1] !== undefined && matrix[rowIndex-1][colIndex-1] !== "X") {
+        matrix[rowIndex-1][colIndex-1] += 1;
+        if (matrix[rowIndex-1][colIndex-1] > 9) {
+            matrix[rowIndex-1][colIndex-1] = "X";
+            matrix = flash(matrix, rowIndex-1, colIndex-1);
+        }
+    }
+    if (matrix?.[rowIndex-1]?.[colIndex] !== undefined && matrix[rowIndex-1][colIndex] !== "X") {
+        matrix[rowIndex-1][colIndex] += 1;
+        if (matrix[rowIndex-1][colIndex] > 9) {
+            matrix[rowIndex-1][colIndex] = "X";
+            matrix = flash(matrix, rowIndex-1, colIndex);
+        }
+    }
+    if (matrix?.[rowIndex-1]?.[colIndex+1] !== undefined && matrix[rowIndex-1][colIndex+1] !== "X") {
+        matrix[rowIndex-1][colIndex+1] += 1;
+        if (matrix[rowIndex-1][colIndex+1] > 9) {
+            matrix[rowIndex-1][colIndex+1] = "X";
+            matrix = flash(matrix, rowIndex-1, colIndex+1);
+        }
+    }
+    if (matrix?.[rowIndex]?.[colIndex-1] !== undefined && matrix[rowIndex][colIndex-1] !== "X") {
+        matrix[rowIndex][colIndex-1] += 1;
+        if (matrix[rowIndex][colIndex-1] > 9) {
+            matrix[rowIndex][colIndex-1] = "X";
+            matrix = flash(matrix, rowIndex, colIndex-1);
+        }
+    }
+    if (matrix?.[rowIndex]?.[colIndex+1] !== undefined && matrix[rowIndex][colIndex+1] !== "X") {
+        matrix[rowIndex][colIndex+1] += 1;
+        if (matrix[rowIndex][colIndex+1] > 9) {
+            matrix[rowIndex][colIndex+1] = "X";
+            matrix = flash(matrix, rowIndex, colIndex+1);
+        }
+    }
+    if (matrix?.[rowIndex+1]?.[colIndex-1] !== undefined && matrix[rowIndex+1][colIndex-1] !== "X") {
+        matrix[rowIndex+1][colIndex-1] += 1;
+        if (matrix[rowIndex+1][colIndex-1] > 9) {
+            matrix[rowIndex+1][colIndex-1] = "X";
+            matrix = flash(matrix, rowIndex+1, colIndex-1);
+        }
+    }
+    if (matrix?.[rowIndex+1]?.[colIndex] !== undefined && matrix[rowIndex+1][colIndex] !== "X") {
+        matrix[rowIndex+1][colIndex] += 1;
+        if (matrix[rowIndex+1][colIndex] > 9) {
+            matrix[rowIndex+1][colIndex] = "X";
+            matrix = flash(matrix, rowIndex+1, colIndex);
+        }
+    }
+    if (matrix?.[rowIndex+1]?.[colIndex+1] !== undefined && matrix[rowIndex+1][colIndex+1] !== "X") {
+        matrix[rowIndex+1][colIndex+1] += 1;
+        if (matrix[rowIndex+1][colIndex+1] > 9) {
+            matrix[rowIndex+1][colIndex+1] = "X";
+            matrix = flash(matrix, rowIndex+1, colIndex+1);
+        }
+    }
+
+    return matrix;
+}
+
+const fs = require("fs");
+const file = fs.readFileSync("./day11/input.txt").toString('utf-8');
+const input = file.split("\n").map((el)=>el.split("").map((char)=>parseInt(char)));
+
+console.log(calculate(input));

7d7d3c53206fcd9bb756e360f87b988c38a7b933
Nobuko <nobuko@next-data-service.com>
2021-12-14 20:29:55 +0100

    WIP2

--- day14/index2.js
+++ day14/index2.js
@@ -6,12 +6,10 @@
  * @return {number}
  */
 const calculate = (template, rules, n) => {
+    const start = Date.now();
     console.log("### template/rules", template, rules)
     const temp = []
     for (let j = 0; j < template.length-1; j++) {
-        // const key = (j*2+1 === result.length) ? result.slice(j*2) : result.slice(j*2, j*2+2);
-        // const insertion = rules.find((rule)=>rule[0] === key);
-        // result = [result.slice(0, j*2+1), insertion[1], result.slice(j*2+1)].join("");
         const chars = j === template.length - 2 ? template.slice(j) : template.slice(j, j+2)
         temp.push(insertNTimes(chars, n, rules));
     }
@@ -27,7 +25,8 @@ const calculate = (template, rules, n) => {
     }
     const countArray = Object.keys(countObject).map((char)=>({char, num: countObject[char]})).sort((a, b)=> a.num - b.num);
     console.log("count", countArray);
-
+    const end = Date.now();
+    console.log("### performance in sec:", (end-start)/1000);
     return countArray[countArray.length-1].num - countArray[0].num;
 };
 
@@ -46,7 +45,7 @@ const insertNTimes = (chars, n, rules) => {
 }
 
 const fs = require("fs");
-const file = fs.readFileSync("./day14/sample.txt").toString('utf-8');
+const file = fs.readFileSync("./day14/input.txt").toString('utf-8');
 const input = file.split("\n");
 
-console.log(calculate(input[0], input.slice(2).map((rule)=>rule.split(" -> ")), 25));
+console.log(calculate(input[0], input.slice(2).map((rule)=>rule.split(" -> ")), 20));

723c8557600361bc2c5ac9f997e12a8a491f2620
Nobuko Yano <nobukoyano31@gmail.com>
2022-12-13 20:44:04 +0100

    day3-task3

--- day3/index2.js
+++ day3/index2.js
@@ -1,30 +1,39 @@
-const scoreMap = [
-    ["A", "X", 3], // loose with Z: 3 + 0
-    ["A", "Y", 4], // draw  with X: 1 + 3
-    ["A", "Z", 8], // win   with Y: 2 + 6
-    ["B", "X", 1], // loose with X: 1 + 0
-    ["B", "Y", 5], // draw  with Y: 2 + 3
-    ["B", "Z", 9], // win   with Z: 3 + 6 
-    ["C", "X", 2], // loose with Y: 2 + 0
-    ["C", "Y", 6], // draw  with Z: 3 + 3
-    ["C", "Z", 7], // win   with X: 1 + 6
-]
-// X: loose, Y: draw, Z: win
 /**
  * day X - first
  * @param {Array<string>} inputs
  * @return {number}
  */
-const task2 = (inputs) => {
-    let score = 0
-    for (const input of inputs) {
-        score += scoreMap.find((score) => score[0] === input[0] && score[1] === input[2])[2];
+const task1 = (inputs) => {
+    const duplicates = [];
+    let i = 0;
+    while (i < (inputs.length - 2)) {
+        duplicates.push(findCommonChar(inputs[i], inputs[i+1], inputs[i+2]));
+        i += 3;
     }
-    return score;
+    return duplicates.reduce((acc, curr)=> acc + getPriority(curr), 0);
 };
 
+const findCommonChar = (a, b, c) => {
+    for (const char of a) {
+        if (b.indexOf(char) !== -1) {
+            if (c.indexOf(char) !== -1) {
+                return char;
+            }
+        }
+    }
+}
+
+const getPriority = (char) => {
+    const temp = char.charCodeAt() - 64;
+    if (temp > 27) {
+        return temp - 32;
+    } else {
+        return temp + 26;
+    }
+
+}
 const fs = require("fs");
-const file = fs.readFileSync("./day2/input.txt").toString('utf-8');
+const file = fs.readFileSync("./day3/input.txt").toString('utf-8');
 const input = file.split("\n")
 
-console.log(task2(input));
+console.log(task1(input));
